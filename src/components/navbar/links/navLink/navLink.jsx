7a4d51b289153aca9f74658082bdb168b8afe1db
Nadezhda Krylova <thenadejdak@gmail.com>
2025-04-11 14:16:50 +0300

    Delete console.log

--- src/components/navbar/links/navLink/navLink.jsx
+++ src/components/navbar/links/navLink/navLink.jsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import styles from "./navLink.module.css"
 
 const NavLink = ({ item, prefix }) => {
-    console.log("item.path = ", item.path)
     
     return (
         <Link data-testid={`${prefix}-${item.path}`} href={item.path}>
