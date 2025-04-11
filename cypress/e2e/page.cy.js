be1a22693b6fb81d85860db015cd8e7f5c9e6abf
Nadezhda Krylova <thenadejdak@gmail.com>
2025-04-11 14:15:04 +0300

    Tests uncommented

--- cypress/e2e/page.cy.js
+++ cypress/e2e/page.cy.js
@@ -1,46 +1,46 @@
 // <reference types="cypress" />
 
 
-// describe('navigation test', () => {
-//   const tickers = ['AAPL', 'GOOG', 'TSLA', 'AMZN', 'MSFT']
+describe('navigation test', () => {
+  const tickers = ['AAPL', 'GOOG', 'TSLA', 'AMZN', 'MSFT']
 
-//   beforeEach(() => {
-//     cy.visit('/')
-//   })
+  beforeEach(() => {
+    cy.visit('/')
+  })
 
-//   it('displays all stock tickers', () => {
-//     tickers.forEach(ticker => {
-//       cy.get(`[data-testid="stock-link-${ticker}"]`).should('be.visible')
-//     })
-//   })
+  it('displays all stock tickers', () => {
+    tickers.forEach(ticker => {
+      cy.get(`[data-testid="stock-link-${ticker}"]`).should('be.visible')
+    })
+  })
 
-//   it('opens StockInfo when AAPL is clicked', () => {
-//     cy.get('[data-testid="stock-link-AAPL"]').click()
-//     cy.get(`[data-testid="stock-details"]`).should('be.visible').and('contain', 'AAPL')
-//   })
+  it('opens StockInfo when AAPL is clicked', () => {
+    cy.get('[data-testid="stock-link-AAPL"]').click()
+    cy.get(`[data-testid="stock-details"]`).should('be.visible').and('contain', 'AAPL')
+  })
 
-//   tickers.forEach(ticker => {
-//     it(`opens StockInfo when ${ticker} is clicked`, () => {
-//       cy.get(`[data-testid="stock-link-${ticker}"]`).click()
-//       cy.get(`[data-testid="stock-details"]`).should('be.visible').and('contain', ticker)
-//     })
-//   })
-// })
+  tickers.forEach(ticker => {
+    it(`opens StockInfo when ${ticker} is clicked`, () => {
+      cy.get(`[data-testid="stock-link-${ticker}"]`).click()
+      cy.get(`[data-testid="stock-details"]`).should('be.visible').and('contain', ticker)
+    })
+  })
+})
 
 describe('Menu test', () => {
 
-  // it('tests opening and closing sidebar menu when button', () => {
+  it('tests opening and closing sidebar menu when button', () => {
 
-  //   cy.viewport(767, 700);
-  //   cy.visit('/');
+    cy.viewport(767, 700);
+    cy.visit('/');
 
-  //   cy.get('[data-testid="mobile-sidebar"]').should('not.be.exist');
-  //   cy.get('[data-testid="open-sidebar-btn"]').should('be.exist').click();
-  //   cy.get('[data-testid="mobile-sidebar"]').should('be.exist');
-  //   cy.get('[data-testid="open-sidebar-btn"]').click();
-  //   cy.get('[data-testid="mobile-sidebar"]').should('not.be.exist');
+    cy.get('[data-testid="mobile-sidebar"]').should('not.be.exist');
+    cy.get('[data-testid="open-sidebar-btn"]').should('be.exist').click();
+    cy.get('[data-testid="mobile-sidebar"]').should('be.exist');
+    cy.get('[data-testid="open-sidebar-btn"]').click();
+    cy.get('[data-testid="mobile-sidebar"]').should('not.be.exist');
 
-  // });
+  });
 
   it('opens links in mobile menu = test navigation', () => {
 
@@ -48,7 +48,7 @@ describe('Menu test', () => {
     cy.visit('/');
 
     cy.get('[data-testid="open-sidebar-btn"]').should('be.exist').click();
-    
+
     cy.get('[data-testid="mobile-sidebar"]').should('be.exist');
 
     cy.get('[data-testid="mobile-link-/"]').should('be.exist').click();
