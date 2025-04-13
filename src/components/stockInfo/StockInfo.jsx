54c1ac6eae0c3e0c2fe5855c11e66faba6737107
Nadezhda Krylova <thenadejdak@gmail.com>
2025-04-13 13:42:40 +0300

    Add data-testid

--- src/components/stockInfo/StockInfo.jsx
+++ src/components/stockInfo/StockInfo.jsx
@@ -5,14 +5,17 @@ import './stockInfo.scss'
 export default function StockInfo({ stock }) {
 
     return (
-        <main className="stockinfo__container">
+        <main data-testid="stock-details" className="stockinfo__container">
             <div className='stockinfo__title'>
                 {stock?.logo && (
                     <div className="stockinfo-logo__wrapper">
                         <img src={stock.logo} alt={`${stock?.name || 'Stock'} logo`} />
                     </div>
                 )}
-                <div className='stockinfo__shortname'><h1 className="stockinfo__h1">{stock.shortName}</h1> <div>({stock.ticker})</div></div>
+                <div className='stockinfo__shortname'>
+                    <h1 className="stockinfo__h1">{stock.shortName}</h1>
+                    <div>({stock.ticker})</div>
+                </div>
             </div>
             <div className='stockinfo__details'>
                 <p><span>Price:</span> ${stock.price}</p>
